38d028185fe4d98dede8b1d5099c855c647e2b6f
astrofrog <thomas.robitaille@gmail.com>
2009-04-28 03:20:28 +0000

    Label styles
    git-svn-id: https://aplpy.svn.sourceforge.net/svnroot/aplpy/trunk/src/APLpy@53 2f3f69f2-b874-4715-96d5-35985bcb53a6

--- aplpy/labels.py
+++ aplpy/labels.py
@@ -5,6 +5,7 @@ import wcs_util
 import numpy as np
 import string
 import math_util
+from matplotlib.font_manager import FontProperties
 
 class Labels(object):
     
@@ -17,8 +18,8 @@ class Labels(object):
         
         # Set default label format
         if system == 'celestial':
-            self._ax1.xaxis.apl_label_form = "hh:mm:ss."
-            self._ax1.yaxis.apl_label_form = "sdd:mm:ss."
+            self._ax1.xaxis.apl_label_form = "hh:mm:ss.ss"
+            self._ax1.yaxis.apl_label_form = "dd:mm:ss.s"
         else:
             self._ax1.xaxis.apl_label_form = "ddd.dddd"
             self._ax1.yaxis.apl_label_form = "dd.dddd"
@@ -47,27 +48,32 @@ class Labels(object):
         fy2 = mpl.NullFormatter()
         self._ax2.xaxis.set_major_formatter(fx2)
         self._ax2.yaxis.set_major_formatter(fy2)
-    
+        
+        # Set font
+        self.font = FontProperties()
+        self._ax1.usetex = False
+        self.set_tick_label_style('plain',refresh=False)
+        
     def set_xlabels_format(self,format,refresh=True):
         '''
         Set the format of the x-axis tick labels
         
         Required Arguments:
-        
+            
             *format*: [ string ]
                 The format for the tick labels. This can be:
-
+                    
                     * ``ddd.ddddd`` - decimal degrees, where the number of decimal places can be varied
                     * ``hh`` or ``dd`` - hours (or degrees)
                     * ``hh:mm`` or ``dd:mm`` - hours and minutes (or degrees and arcminutes)
                     * ``hh:mm:ss`` or ``dd:mm:ss`` - hours, minutes, and seconds (or degrees, arcminutes, and arcseconds)
-                    * ``hh:mm:ss.ss`` or ``dd:mm:ss.ss`` - hours, minutes, and seconds (or degrees, arcminutes, and arcseconds), where the number of decimal places can be varied. 
-                    
+                    * ``hh:mm:ss.ss`` or ``dd:mm:ss.ss`` - hours, minutes, and seconds (or degrees, arcminutes, and arcseconds), where the number of decimal places can be varied.
+        
         Optional Keyword Arguments:
-
+            
             *refresh*: [ True | False ]
                 Whether to refresh the display straight after setting the parameter.
-                For non-interactive uses, this can be set to False.                                         
+                For non-interactive uses, this can be set to False.
         '''
         
         self._ax1.xaxis.apl_label_form = format
@@ -78,21 +84,21 @@ class Labels(object):
         Set the format of the x-axis tick labels
         
         Required Arguments:
-        
+            
             *format*: [ string ]
                 The format for the tick labels. This can be:
-
+                    
                     * ``ddd.ddddd`` - decimal degrees, where the number of decimal places can be varied
                     * ``hh`` or ``dd`` - hours (or degrees)
                     * ``hh:mm`` or ``dd:mm`` - hours and minutes (or degrees and arcminutes)
                     * ``hh:mm:ss`` or ``dd:mm:ss`` - hours, minutes, and seconds (or degrees, arcminutes, and arcseconds)
-                    * ``hh:mm:ss.ss`` or ``dd:mm:ss.ss`` - hours, minutes, and seconds (or degrees, arcminutes, and arcseconds), where the number of decimal places can be varied.  
-            
+                    * ``hh:mm:ss.ss`` or ``dd:mm:ss.ss`` - hours, minutes, and seconds (or degrees, arcminutes, and arcseconds), where the number of decimal places can be varied.
+        
         Optional Keyword Arguments:
-
+            
             *refresh*: [ True | False ]
                 Whether to refresh the display straight after setting the parameter.
-                For non-interactive uses, this can be set to False.            
+                For non-interactive uses, this can be set to False.
         '''
         
         self._ax1.yaxis.apl_label_form = format
@@ -108,6 +114,52 @@ class Labels(object):
         self._ax1.set_ylabel('(Dec. J2000)',family=family,fontsize=fontsize,fontstyle=fontstyle)
         
         if refresh: self.refresh()
+    
+    def set_tick_label_style(self,style,refresh=True):
+        """
+        Set the format of the x-axis tick labels
+        
+        Required Arguments:
+            
+            *style*: [ 'colons' | 'plain' | 'latex' ]
+            
+            How to style the tick labels:
+            
+            'colons' uses colons as separators, for example
+            31:41:59.26 +27:18:28.1
+            
+            'plain' uses plain letters as separators, for example
+            31:41:59.26 +27:18:28.1
+            
+            'latex' uses superscripts, and typesets the labels
+            with LaTeX. To decide whether to use the matplotlib
+            internal LaTeX engine or a real LaTeX installation, use
+            the set_latex() method.
+        
+        """
+        
+        if not style in ['colons','plain','latex']:
+            raise Exception("Label style should be one of colons/plain/latex")
+        
+        self._ax1.xaxis.apl_label_style = style
+        self._ax1.yaxis.apl_label_style = style
+    
+        if refresh:
+            self.refresh()
+    
+    def set_font(self,family=None,usetex=None):
+        
+        if family:
+            self.font.set_family(family)
+        if usetex:
+            self._ax1.usetex = usetex
+        
+        for tick in self._ax1.get_xticklabels():
+            tick.set_fontproperties(self.font)
+        for tick in self._ax1.get_yticklabels():
+            tick.set_fontproperties(self.font)
+        
+        self.refresh()
 
 class WCSFormatter(mpl.Formatter):
     
@@ -120,18 +172,24 @@ class WCSFormatter(mpl.Formatter):
         
         hours = 'h' in self.axis.apl_label_form
         
-        if hours:
-            sep = ('h','m','s')
-        else:
+        if self.axis.apl_label_style == 'plain':
             sep = ('d','m','s')
+            if hours: sep = ('h','m','s') 
+        elif self.axis.apl_label_style == 'colons':
+            sep = (':',':','')
+        elif self.axis.apl_label_style == 'latex':
+            if hours:
+                sep = ('^{h}','^{m}','^{s}')
+            else:
+                sep = ('^{\circ}','^{\prime}','^{\prime\prime}')
         
         ymin, ymax = self.axis.get_axes().yaxis.get_view_interval()
         xmin, xmax = self.axis.get_axes().xaxis.get_view_interval()
-            
+        
         ipos = math_util.minloc(np.abs(self.axis.apl_tick_positions_pix-x))
         
         c = self.axis.apl_tick_spacing * self.axis.apl_tick_positions_world[ipos]
-
+        
         if hours:
             c = c.tohours()
         
@@ -148,5 +206,8 @@ class WCSFormatter(mpl.Formatter):
                     cnext.pop(0)
                 else:
                     break
-            
-        return string.join(c,"")
+        
+        if self.axis.apl_label_style == 'latex':
+            return "$"+string.join(c,"")+"$"
+        else:
+            return string.join(c,"")
