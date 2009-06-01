86d50335b1a5af34c81caa671a90b6153919e18a
astrofrog <thomas.robitaille@gmail.com>
2009-06-01 22:19:19 +0000

    Release 0.9.1.2, removed code from header.py that was attempting to fix things for MSX images but causing bugs for other -CAR images
    git-svn-id: https://aplpy.svn.sourceforge.net/svnroot/aplpy/trunk/src/APLpy@93 2f3f69f2-b874-4715-96d5-35985bcb53a6

--- aplpy/header.py
+++ aplpy/header.py
@@ -25,25 +25,6 @@ def check(header):
     if xproj<>yproj:
         raise Exception("x and y projections do not agree")
     
-    # If projection is -CAR, then make sure the projection center is within the image
-    
-    if xproj == '-CAR':
-        if crpix1 < 0.5 or crpix1 > nx + 0.5 or crpix2 < 0.5 or crpix2 > ny + 0.5:
-            xcw,ycw = wcs_util.pix2world(wcs,xcp,ycp)
-            header.update('CRVAL1',xcw)
-            header.update('CRVAL2',ycw)
-            header.update('CRPIX1',xcp)
-            header.update('CRPIX2',ycp)
-            if header.has_key('LONPOLE'):
-                lonpole = float(header['LONPOLE'])
-                if lonpole == 0. or lonpole == 180.:
-                    if ycw >= 0.:
-                        header.update('LONPOLE',0.)
-                    else:
-                        header.update('LONPOLE',180.)
-            
-            print "[check] updated projection center"
-    
     # Remove any extra coordinates
     for key in ['CTYPE','CRPIX','CRVAL','CUNIT','CDELT']:
         for coord in range(3,5):
