934351b872cecb14ff707ea16a2df26de63d00c2
astrofrog <thomas.robitaille@gmail.com>
2009-08-24 00:19:53 +0000

    - added docstring for beam method
    git-svn-id: https://aplpy.svn.sourceforge.net/svnroot/aplpy/trunk/src/APLpy@158 2f3f69f2-b874-4715-96d5-35985bcb53a6

--- aplpy/overlays.py
+++ aplpy/overlays.py
@@ -9,6 +9,45 @@ class Beam(object):
     def show_beam(self, major='BMAJ', minor='BMIN', \
         angle='BPA', corner=3, frame=False, **kwargs):
 
+        '''
+        Display the beam shape and size for the primary image
+
+        By default, this method will search for the BMAJ, BMIN, and BPA
+        keywords in the FITS header to set the major and minor axes and the
+        position angle on the sky.
+
+        Optional Keyword Arguments:
+
+            *major*: [ float ]
+                Major axis of the beam in degrees (overrides BMAJ if present)
+
+            *minor*: [ float ]
+                Minor axis of the beam in degrees (overrides BMIN if present)
+
+            *angle*: [ float ]
+                Position angle of the beam on the sky in degrees (overrides
+                BPA if present)
+
+            *corner*: [ integer ]
+                Where to place the beam. Acceptable values are:
+                1: top right
+                2: top left
+                3: bottom left (default)
+                4: bottom right
+                5: center right
+                6: center left
+                7: center right (same as 5)
+                8: bottom center
+                9: top center
+
+            *frame*: [ True | False ]
+                Whether to display a frame behind the beam (default is False)
+
+            Additional arguments can be used to control the appearance of the
+            beam (more information soon)
+
+        '''
+
         if type(major) == str:
             major = self._hdu.header[major]
 
@@ -39,6 +78,9 @@ class Beam(object):
         self.refresh()
 
     def hide_beam(self):
+        '''
+        Hide the beam
+        '''
         try:
             self._ax1._beam.remove()
         except:
