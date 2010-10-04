11d071442a458cbbfce9904ec6c360a8afc0fcfe
astrofrog <thomas.robitaille@gmail.com>
2010-10-04 15:56:17 +0000

    0.9.5 candidate
    git-svn-id: https://aplpy.svn.sourceforge.net/svnroot/aplpy/trunk/src/APLpy@263 2f3f69f2-b874-4715-96d5-35985bcb53a6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='APLpy',
-      version='0.9.4',
+      version='0.9.5',
       description='The Astronomical Plotting Library in Python',
       author='Eli Bressert and Thomas Robitaille',
       author_email='elibre@users.sourceforge.net, robitaille@users.sourceforge.net',
