e6de8eb1b003c7801027cadf4489af34e1fa5269
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-06-06 14:55:52 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,27 @@
 
 from distutils.core import setup
 
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
 setup(name='APLpy',
       version='0.9.5',
       description='The Astronomical Plotting Library in Python',
-      author='Eli Bressert and Thomas Robitaille',
-      author_email='elibre@users.sourceforge.net, robitaille@users.sourceforge.net',
-      url='http://aplpy.sourceforge.net/',
-      packages=['aplpy']
+      author='Thomas Robitaille and Eli Bressert',
+      author_email='thomas.robitaille@gmail.com, elibre@users.sourceforge.net',
+      license='MIT',
+      url='http://aplpy.github.com/',
+      download_url='https://github.com/downloads/aplpy/aplpy/APLpy-0.9.5.tar.gz',
+      packages=['aplpy'],
+      provides=['aplpy'],
+      requires=['pywcs', 'pyfits', 'numpy', 'matplotlib'],
+      cmdclass={'build_py': build_py},
+      keywords=['Scientific/Engineering'],
+      classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: MIT License",
+                  ],
      )
