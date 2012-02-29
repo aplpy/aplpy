ace52bbd3c988095eae9eff8e010e17eee972535
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-02-29 14:20:03 +0100

    Set backend to Agg for new tests

--- tests/test_beam.py
+++ tests/test_beam.py
@@ -1,10 +1,13 @@
+import matplotlib
+matplotlib.use('Agg')
+
 import numpy as np
 import pytest
 
 import aplpy
 
 # Test simple contour generation with Numpy example
-def test_numpy_beam_invalid():
+def test_numpy_beam():
     data = np.arange(256).reshape((16, 16))
     f = aplpy.FITSFigure(data)
     f.show_grayscale()
