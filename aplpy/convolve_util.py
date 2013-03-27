61e7368f289fffb6f6b5a25ae5c8671dc3fecd8d
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-03-27 11:51:47 +0100

    No longer accept tuples - just require the user to pass the kernel if it's not a symmetric box function or Gaussian kernel.

--- aplpy/convolve_util.py
+++ aplpy/convolve_util.py
@@ -6,6 +6,11 @@ from astropy.nddata import convolve as nddata_convolve, make_kernel
 
 def convolve(image, smooth=3, kernel='gauss'):
 
+    if not np.isscalar(smooth):
+        raise ValueError("smooth= should be an integer - for more complex "
+                         "kernels, pass an array containing the kernel "
+                         "to the kernel= option")
+
     # The Astropy convolution doesn't treat +/-Inf values correctly yet, so we
     # convert to NaN here.
 
