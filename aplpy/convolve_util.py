f362b4b9f519705f9bff641a8c10b6c66f75caa0
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-03-27 11:39:28 +0100

    Improved convolution for smoothing

--- aplpy/convolve_util.py
+++ aplpy/convolve_util.py
@@ -1,9 +1,16 @@
 from __future__ import absolute_import, print_function, division
 
+import numpy as np
 from astropy.nddata import convolve as nddata_convolve, make_kernel
 
 
 def convolve(image, smooth=3, kernel='gauss'):
+
+    # The Astropy convolution doesn't treat +/-Inf values correctly yet, so we
+    # convert to NaN here.
+
+    image_fixed = image.copy()
+    image_fixed[np.isinf(image)] = np.nan
 
     if smooth is None:
         return image
@@ -15,4 +22,4 @@ def convolve(image, smooth=3, kernel='gauss'):
     else:
         kernel = kernel
 
-    return nddata_convolve(image, kernel)
+    return nddata_convolve(image, kernel, boundary='extend')
