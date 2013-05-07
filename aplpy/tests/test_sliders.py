9d9fcc97e5574834606e6ec78750682deb2477da
Adam Ginsburg <keflavich@gmail.com>
2013-05-07 00:48:13 -0600

    expanded test_sliders tests a little

--- aplpy/tests/test_sliders.py
+++ aplpy/tests/test_sliders.py
@@ -13,5 +13,19 @@ def test_add_sliders():
     F.show_sliders()
     fignum = F.Sliders.toolfig.number
     F.show_sliders()
+    # make sure showing sliders twice doesn't make a new instance
     assert F.Sliders.toolfig.number == fignum
+    F.hide_sliders()
+
+
+def test_add_log_sliders():
+    data = np.random.randn(16, 16)
+    F = FITSFigure(data)
+    F.show_grayscale(stretch='log')
+    F.show_sliders()
+    fignum = F.Sliders.toolfig.number
+    F.show_sliders()
+    # make sure showing sliders twice doesn't make a new instance
+    assert F.Sliders.toolfig.number == fignum
+    F.hide_sliders()
 
