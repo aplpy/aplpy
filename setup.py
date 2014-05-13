614e088cd7b22a2a2195fd29a4f38114f8383708
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-05-13 14:22:39 +0200

    Hard-code distribution name

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ for root, dirs, files in os.walk(PACKAGENAME):
                     os.path.relpath(root, PACKAGENAME), filename))
 package_info['package_data'][PACKAGENAME].extend(c_files)
 
-setup(name=PACKAGENAME,
+setup(name='APLpy',
       version=VERSION,
       description=DESCRIPTION,
       scripts=scripts,
