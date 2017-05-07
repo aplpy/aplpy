527ab3f8f8f13b52eeca4d51bd16dd7f9a08ec8f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-07 17:49:56 +0200

    WCS.naxis, not data.naxis

--- aplpy/core.py
+++ aplpy/core.py
@@ -221,8 +221,8 @@ class FITSFigure(Layers, Regions, Deprecated):
             data.wcs.crpix = [data.wcs.crpix[0] / nx * float(nx), data.wcs.crpix[1] / ny * float(ny)]
 
             # Update the NAXIS values with the true dimensions of the RGB image
-            data.naxis1 = data.nx = nx
-            data.naxis2 = data.ny = ny
+            data.wcs.naxis1 = data.nx = nx
+            data.wcs.naxis2 = data.ny = ny
 
         if isinstance(data, WCS_TYPES):
             wcs = data
