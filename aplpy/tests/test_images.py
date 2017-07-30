d31b7a2e94395ec13c737e4a4c649a89ce2f2af0
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-30 10:17:06 +0100

    Enforce variable style

--- aplpy/tests/test_images.py
+++ aplpy/tests/test_images.py
@@ -41,14 +41,14 @@ class TestBasic(BaseImageTests):
 
     # Test for showing grayscale
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=7.5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=7.5)
     def test_basic_image(self, generate):
         f = FITSFigure(self.filename_2, figsize=(7, 5))
         f.show_grayscale()
         return f._figure
 
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=7.5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=7.5)
     def test_ticks_labels_options(self, generate):
         f = FITSFigure(self.filename_2, figsize=(7, 5))
 
@@ -71,7 +71,7 @@ class TestBasic(BaseImageTests):
 
     # Test for showing colorscale
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
     def test_show_colorbar_scalebar_beam(self, generate):
         f = FITSFigure(self.filename_1, figsize=(7, 5))
         f.ticks.set_color('black')
@@ -84,7 +84,7 @@ class TestBasic(BaseImageTests):
 
     # Test for overlaying shapes
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=1.5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=1.5)
     def test_overlay_shapes(self, generate):
         f = FITSFigure(self.filename_1, figsize=(7, 5))
 
@@ -106,7 +106,7 @@ class TestBasic(BaseImageTests):
 
     # Test for grid
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=7.5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=7.5)
     def test_grid(self, generate):
         f = FITSFigure(self.filename_1, figsize=(7, 5))
 
@@ -125,7 +125,7 @@ class TestBasic(BaseImageTests):
 
     # Test recenter
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=1.5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=1.5)
     def test_recenter(self, generate):
         f = FITSFigure(self.filename_2, figsize=(7, 5))
 
@@ -141,7 +141,7 @@ class TestBasic(BaseImageTests):
 
     # Test overlaying contours
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
     def test_contours(self, generate):
         data = np.arange(256).reshape((16, 16))
         f = FITSFigure(data, figsize=(7, 5))
@@ -156,7 +156,7 @@ class TestBasic(BaseImageTests):
 
     # Test cube slice
     @remote_data
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
     def test_cube_slice(self, generate):
         f = FITSFigure(self.filename_3, dimensions=[2, 0], slices=[10], figsize=(7, 5), subplot=[0.25, 0.1, 0.7, 0.8])
         f.ticks.set_color('black')
@@ -172,7 +172,7 @@ class TestBasic(BaseImageTests):
     # Test for ds9 regions
     @remote_data
     @pytest.mark.skipif("not PYREGION_INSTALLED")
-    @pytest.mark.mpl_image_compare(savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
+    @pytest.mark.mpl_image_compare(style={}, savefig_kwargs={'adjust_bbox': False}, baseline_dir=baseline_dir, tolerance=5)
     def test_regions(self, generate):
         f = FITSFigure(self.filename_2, figsize=(7, 5))
 
