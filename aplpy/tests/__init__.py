d34e783b7171ac6f4ceaec1f8958ea0c27b1384f
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-30 17:24:14 +0100

    Updated reference image URL

--- aplpy/tests/__init__.py
+++ aplpy/tests/__init__.py
@@ -4,7 +4,7 @@ import matplotlib
 
 MPL_VERSION = LooseVersion(matplotlib.__version__)
 
-ROOT = "http://astrofrog.github.io/aplpy-data/2.x/2017-07-29T21:57:08.143"
+ROOT = "http://aplpy.github.io/aplpy-data/2.x/2017-07-29T21:57:08.143"
 
 if MPL_VERSION >= LooseVersion('2.0.0'):
     baseline_dir = ROOT + '/2.0.x/'
