0cd6fcd43733dfb5b61c1c6f7e35c89d765cf033
e-koch <koch.eric.w@gmail.com>
2018-07-11 16:40:55 -0600

    Add rasterize to contour test

--- aplpy/tests/test_contour.py
+++ aplpy/tests/test_contour.py
@@ -2,16 +2,19 @@ from __future__ import absolute_import, print_function, division
 
 import numpy as np
 from astropy.tests.helper import pytest
+from itertools import product
 
 from .. import FITSFigure
 
 # Test simple contour generation with Numpy example
 
 
-@pytest.mark.parametrize(('filled'), [True, False])
-def test_numpy_contour(filled):
+@pytest.mark.parametrize(('filled', 'rasterize'),
+                         product([True, False], [True, False]))
+def test_numpy_contour(filled, rasterize):
     data = np.arange(256).reshape((16, 16))
     f = FITSFigure(data)
     f.show_grayscale()
-    f.show_contour(data, levels=np.linspace(1., 254., 10), filled=filled)
+    f.show_contour(data, levels=np.linspace(1., 254., 10), filled=filled,
+                   rasterize=rasterize)
     f.close()
