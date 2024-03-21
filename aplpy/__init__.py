97c87c434e8875424837b0e256bad541ee9163d8
Thomas Robitaille <thomas.robitaille@gmail.com>
2024-03-21 14:47:37 +0000

    Migrated setup.py/setup.cfg to pyproject.toml

--- aplpy/__init__.py
+++ aplpy/__init__.py
@@ -3,20 +3,14 @@
 APLpy : Astronomical Plotting Library in Python
 """
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-__version__ = ''
-_ASTROPY_SETUP_ = False
-if not _ASTROPY_SETUP_:  # noqa
+from .core import FITSFigure  # noqa
+from .rgb import make_rgb_image, make_rgb_cube  # noqa
 
-    from .core import FITSFigure  # noqa
-    from .rgb import make_rgb_image, make_rgb_cube  # noqa
-
-    from .frame import Frame  # noqa
-    from .overlays import Scalebar, Beam  # noqa
-    from .colorbar import Colorbar  # noqa
-    from .grid import Grid  # noqa
-    from .ticks import Ticks  # noqa
-    from .tick_labels import TickLabels  # noqa
-    from .axis_labels import AxisLabels  # noqa
+from .frame import Frame  # noqa
+from .overlays import Scalebar, Beam  # noqa
+from .colorbar import Colorbar  # noqa
+from .grid import Grid  # noqa
+from .ticks import Ticks  # noqa
+from .tick_labels import TickLabels  # noqa
+from .axis_labels import AxisLabels  # noqa
+from .version import __version__  # noqa
